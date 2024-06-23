21ceb32526aee3f0232a8766a0f2dcfe99c27ac2
Skyler McCloud <39038511+Heroshrine@users.noreply.github.com>
2024-06-23 00:31:14 -0700

    changes preppinng for re-write

---
+++ Runtime/Scripts/ChanceGenMono.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class ChanceGenMono : MonoBehaviour
+{
+    public int size;
+    public float randomRoomChanceDecrease;
+    public int regenerateLimit;
+    public int shrinkLimit;
+
+    [Space] public ConwayRules removeRules;
+    public ConwayRules addRules;
+    public SpecialRoomRules specialRoomRules;
+    public DebugInfoSettings debugInfoSettings;
+
+    public string customSeed;
+
+    ChanceGenerator current;
+
+    public bool CurrentFinished => (current == null || !current.isGenerating);
+
+    //public void Stop() => current.Destroy();
+
+    public void Generate()
+    {
+        if (CurrentFinished)
+        {
+            //current = ChanceGenerator.GetOrCreate();
+
+            current.Dispose();
+
+            current.Generate(new GenerationInfo(1, size, randomRoomChanceDecrease, regenerateLimit, shrinkLimit),
+                removeRules, addRules, specialRoomRules, debugInfoSettings);
+        }
+    }
+}
